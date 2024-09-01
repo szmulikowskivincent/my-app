a7ed4bf5b74c267f9c7bb8287a985b1a50e99e88
szmulikowski vincent <szmulikowski.vincent.1971@hotmail.com>
2024-09-01 13:04:19 +0200

    $ git push --force-with-lease

--- src/components/userProfile.js
+++ src/components/userProfile.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import "../css/userProfile.css";
 import myPhoto from "../assets/avatar1.png";
 
+const defaultAvatar1 = "https://via.placeholder.com/40?text=A";
+const defaultAvatar2 = "https://via.placeholder.com/40?text=B";
+const defaultAvatar3 = "https://via.placeholder.com/40?text=C";
+
 const UserProfile = ({ user }) => {
   const [posts, setPosts] = useState([
     {
@@ -46,101 +50,131 @@ const UserProfile = ({ user }) => {
     }
   };
 
+  const [friends] = useState([
+    { name: "Alice", avatar: defaultAvatar1, status: "online" },
+    { name: "Bob", avatar: defaultAvatar2, status: "offline" },
+    { name: "Charlie", avatar: defaultAvatar3, status: "online" },
+  ]);
+
+  const getStatusClass = (status) => {
+    return status === "online" ? "status-online" : "status-offline";
+  };
+
   return (
-    <div className="user-profile-container">
-      <div className="user-info">
-        <img
-          src={myPhoto}
-          alt="My Profile"
-          className="profile-photo"
-          style={{ width: "130px" }}
-        />
-        <h2>{user.name}</h2>
-        <p>{user.bio}</p>
-      </div>
-
-      <div className="posts-container">
-        <h3>Publications</h3>
-        <ul className="posts-list">
-          {posts.map((post, index) => (
-            <li key={index} className="post-item">
-              <p>{post.content}</p>
-
-              {index === 0 && (
-                <div className="post-video">
-                  <iframe
-                    width="560"
-                    height="315"
-                    src="https://www.youtube.com/embed/WDhz5h8BlsQ"
-                    title="YouTube video player"
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                  ></iframe>
-                </div>
-              )}
-
-              <div className="post-likes">
-                <button className="like-button">👍 {post.likes}</button>
-              </div>
-
-              {index === 0 && (
-                <div className="post-comments" style={{ marginTop: "0.5cm" }}>
-                  <textarea
-                    placeholder="Add a comment..."
-                    className="comment-input"
-                    style={{ width: "540px" }}
-                    value={newComment}
-                    onChange={(e) => setNewComment(e.target.value)}
-                  ></textarea>
-                  <button
-                    onClick={() => handleAddComment(index)}
-                    className="comment-submit"
-                  >
-                    Submit
-                  </button>
-                </div>
-              )}
-
-              <div className="comments-list">
-                {postComments[index].map((comment, commentIndex) => (
-                  <p key={commentIndex} className="comment-item">
-                    {comment}
-                  </p>
-                ))}
-              </div>
+    <div className="user-profile-page">
+      <div className="friends-container">
+        <h3>Amis</h3>
+        <ul className="friends-list">
+          {friends.map((friend, index) => (
+            <li key={index} className="friend-item">
+              <img
+                src={friend.avatar}
+                alt={`${friend.name}'s avatar`}
+                className="friend-avatar"
+              />
+              <span
+                className={`friend-status ${getStatusClass(friend.status)}`}
+              ></span>
+              <p className="friend-name">{friend.name}</p>
             </li>
           ))}
         </ul>
       </div>
 
-      <div className="chat-container">
-        <div className="chat-header">
-          <span>Chat</span>
-          <button className="close-button">×</button>
-        </div>
-        <div className="chat-box">
-          {messages.map((msg, index) => (
-            <div
-              key={index}
-              className={`message ${
-                msg.sender === "Moi" ? "user-message" : "contact-message"
-              }`}
-            >
-              <p>
-                <strong>{msg.sender}:</strong> {msg.text}
-              </p>
-            </div>
-          ))}
-        </div>
-        <div className="chat-input">
-          <input
-            type="text"
-            value={newMessage}
-            onChange={(e) => setNewMessage(e.target.value)}
-            placeholder="Type a message..."
+      <div className="user-profile-container">
+        <div className="user-info">
+          <img
+            src={myPhoto}
+            alt="My Profile"
+            className="profile-photo"
+            style={{ width: "150px" }}
           />
-          <button onClick={handleSendMessage}>Send</button>
+          <h2>{user.name}</h2>
+          <p>{user.bio}</p>
+        </div>
+        <div className="posts-container">
+          <h3>Publications</h3>
+          <ul className="posts-list">
+            {posts.map((post, index) => (
+              <li key={index} className="post-item">
+                <p>{post.content}</p>
+
+                {index === 0 && (
+                  <div className="post-video">
+                    <iframe
+                      width="560"
+                      height="315"
+                      src="https://www.youtube.com/embed/WDhz5h8BlsQ"
+                      title="YouTube video player"
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                    ></iframe>
+                  </div>
+                )}
+
+                <div className="post-likes">
+                  <button className="like-button">👍 {post.likes}</button>
+                </div>
+
+                {index === 0 && (
+                  <div className="post-comments" style={{ marginTop: "0.5cm" }}>
+                    <textarea
+                      placeholder="Add a comment..."
+                      className="comment-input"
+                      style={{ width: "540px" }}
+                      value={newComment}
+                      onChange={(e) => setNewComment(e.target.value)}
+                    ></textarea>
+                    <p />
+                    <button
+                      onClick={() => handleAddComment(index)}
+                      className="comment-submit"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                )}
+
+                <div className="comments-list">
+                  {postComments[index].map((comment, commentIndex) => (
+                    <p key={commentIndex} className="comment-item">
+                      {comment}
+                    </p>
+                  ))}
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="chat-container">
+          <div className="chat-header">
+            <span>Chat</span>
+            <button className="close-button">×</button>
+          </div>
+          <div className="chat-box">
+            {messages.map((msg, index) => (
+              <div
+                key={index}
+                className={`message ${
+                  msg.sender === "Moi" ? "user-message" : "contact-message"
+                }`}
+              >
+                <p>
+                  <strong>{msg.sender}:</strong> {msg.text}
+                </p>
+              </div>
+            ))}
+          </div>
+          <div className="chat-input">
+            <input
+              type="text"
+              value={newMessage}
+              onChange={(e) => setNewMessage(e.target.value)}
+              placeholder="Type a message..."
+            />
+            <button onClick={handleSendMessage}>Send</button>
+          </div>
         </div>
       </div>
     </div>
